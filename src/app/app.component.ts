7920a159d10256186934b4bd154693e5687d0105
DeivaSubramaniam <deivamanimca@gmail.com>
2017-12-18 17:44:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChildComponent } from '../app/child/child.component';
+import { DataService } from './shared/data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'ParentApp-works!!!';
+  myFabFood = 'Biriyani';
+  Myname = 'Deivamani';
+  message: string;
+  constructor(private ds: DataService) {}
+ngOnInit() {
+   this.ds.currentMessage.subscribe(message => this.message = message);
+}
+  // updateFromChild($event) {
+  //   this.count++;
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { SiblingComponent } from './child/sibling/sibling.component';
+import { DataService } from './shared/data.service';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    SiblingComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
